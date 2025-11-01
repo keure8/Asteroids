b11ea229a02d353b1bf0466df6ee2f6ec7b2009f
keure8 <keure8@gmail.com>
2025-11-01 15:14:48 -0400

    added groups updatable and drawable

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import *
 from player import Player
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+
 def main():
     pygame.init()
     
@@ -14,14 +18,20 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
